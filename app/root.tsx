d10fbf1073f0736d118b34f5d9e54de78c71f65e
SebastianMirage <smirage05@gmail.com>
2026-05-01 01:20:10 -0500

    commit

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
 import type { Route } from "./+types/root"
 import "./app.css"
 
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,6 @@ import {
   Field,
   FieldLabel,
   FieldGroup,
-  FieldError,
   FieldDescription,
   FieldContent,
 } from "~/components/ui/field"
@@ -10,54 +9,103 @@ import { Input } from "~/components/ui/input"
 import { Button } from "~/components/ui/button"
 import { Root as RadioGroup } from "@radix-ui/react-radio-group"
 import { RadioGroupItem } from "~/components/ui/radio-group"
+import { useState } from "react"
 
 export default function Home() {
-  function handleSubmit() {
-    console.log("Enviado")
+  const [audioFile, setAudioFile] = useState<File | null>(null)
+  const [radioOption, setRadioOption] = useState<string>("sala-concierto")
+  const [audioURL, setAudioURL] = useState("")
+  const [isAudio, setIsAudio] = useState(false)
+
+  const handleSubmit = async () => {
+    if (!audioFile) return
+
+    //Crear datos del formularo
+    const formData = new FormData()
+    //Campos del formulario
+    formData.append("file", audioFile)
+
+    const response = await fetch(
+      `http://localhost:3000/api/file/${radioOption}`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+
+    if (!response) {
+      throw new Error("Sin respuesta")
+    }
+
+    if (response.status === 200) {
+      const audioBlob = await response.blob()
+      const url = URL.createObjectURL(audioBlob)
+      setAudioURL(url)
+      setIsAudio(true)
+      console.log("ok")
+      return
+    }
+  }
+
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0] ?? null
+    setAudioFile(file)
   }
 
   return (
-    <div className="flex min-h-screen max-w-full p-6">
-      <FieldGroup>
-        <Field>
-          <FieldLabel htmlFor="input">Hola</FieldLabel>
-          <Input
-            id="file-input"
-            type="file"
-            accept=".wav, .aac, mp3, .flac"
-          ></Input>
-          <FieldDescription>Sube tu archivo de audio</FieldDescription>
-        </Field>
-        <RadioGroup defaultValue="comfortable" className="w-fit">
-          <Field orientation="horizontal">
-            <RadioGroupItem value="default" id="desc-r1" />
-            <FieldContent>
-              <FieldLabel htmlFor="desc-r1">Default</FieldLabel>
-              <FieldDescription>
-                Standard spacing for most use cases.
-              </FieldDescription>
-            </FieldContent>
+    <div className="container flex max-w-screen items-center justify-center">
+      <div className="min-w-lg p-6">
+        <FieldGroup className="py-4">
+          <Field>
+            <FieldLabel htmlFor="input">Audio de entrada</FieldLabel>
+            <Input
+              id="file-input"
+              type="file"
+              accept=".wav"
+              onChange={handleInput}
+            ></Input>
+            <FieldDescription>Sube tu archivo de audio</FieldDescription>
           </Field>
-          <Field orientation="horizontal">
-            <RadioGroupItem value="comfortable" id="desc-r2" />
-            <FieldContent>
-              <FieldLabel htmlFor="desc-r2">Comfortable</FieldLabel>
-              <FieldDescription>More space between elements.</FieldDescription>
-            </FieldContent>
-          </Field>
-          <Field orientation="horizontal">
-            <RadioGroupItem value="compact" id="desc-r3" />
-            <FieldContent>
-              <FieldLabel htmlFor="desc-r3">Compact</FieldLabel>
-              <FieldDescription>
-                Minimal spacing for dense layouts.
-              </FieldDescription>
-            </FieldContent>
-          </Field>
-        </RadioGroup>
+          <RadioGroup
+            defaultValue="sala-concierto"
+            className="w-fit"
+            onValueChange={(value: string) => setRadioOption(value)}
+          >
+            <Field orientation="horizontal" className="py-2">
+              <RadioGroupItem value="sala-concierto" id="desc-r1" />
+              <FieldContent>
+                <FieldLabel htmlFor="desc-r1">Sala de conciertos</FieldLabel>
+                <FieldDescription>
+                  Respuesta impulsional de una sala de conciertos. Reverberación
+                  moderada:
+                </FieldDescription>
+              </FieldContent>
+            </Field>
+            <Field orientation="horizontal" className="py-2">
+              <RadioGroupItem value="catedral" id="desc-r2" />
+              <FieldContent>
+                <FieldLabel htmlFor="desc-r2">Catedral</FieldLabel>
+                <FieldDescription>
+                  Respuesta impulsional de una catedral. Reverberación fuerte.
+                </FieldDescription>
+              </FieldContent>
+            </Field>
+            <Field orientation="horizontal" className="py-2">
+              <RadioGroupItem value="cuarto" id="desc-r3" />
+              <FieldContent>
+                <FieldLabel htmlFor="desc-r3">Cuarto pequeño</FieldLabel>
+                <FieldDescription>
+                  Respuesta impulsional para una habitación pequeña. Atenuación
+                  leve.
+                </FieldDescription>
+              </FieldContent>
+            </Field>
+          </RadioGroup>
 
-        <Button onClick={handleSubmit}>Envíar</Button>
-      </FieldGroup>
+          <Button onClick={handleSubmit}>Envíar</Button>
+        </FieldGroup>
+        <div>{isAudio && <audio controls src={audioURL}></audio>}</div>
+      </div>
     </div>
   )
 }
